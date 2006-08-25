b3dda019ea5e26a2ec8dbd64de12bded783eae29
Christian Luijten <islandsvinur@users.noreply.github.com>
2006-08-25 15:06:19 +0000

    This sieve does not do anything different than the 'cat' program: It just outputs the file given at the command line.

--- src/Sieve.java
+++ src/Sieve.java
@@ -4,8 +4,29 @@
  */
 
 import amber.sieve.KeywordSpotter;
+import java.io.*;
 
 /* Starts the parser */
 public class Sieve {
+    public static void main(String args[]) {
+        if (args.length == 1) {
+            
+            try {
 
+                FileInputStream fp = new FileInputStream(args[0]);
+                DataInputStream in = new DataInputStream(fp);
+
+                while (in.available() != 0) {
+                    System.out.println(in.readLine());
+                }
+
+                // KeywordSpotter spotter = new KeywordSpotter();
+            } catch (Exception e) {
+                System.err.println("File input error");
+            }
+
+        } else {
+            System.err.println("Argument invalid");
+        }
+    }
 }
