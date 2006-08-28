2b7cf75ed41c36642a0ec826ffb99141f507d957
christian06@ru.is <christian06@ru.is@05a5482f-ad1b-0410-9432-f6b432cbd593>
2006-08-28 12:07:00 +0000

    Spielereien

--- trunk/src/amber/sieve/KeywordSpotter.java
+++ trunk/src/amber/sieve/KeywordSpotter.java
@@ -1,5 +1,23 @@
 
 package amber.sieve;
 
+import com.cmlabs.air.*;
+
 public class KeywordSpotter {
+
+    private JavaAIRPlug plug;
+
+    public KeywordSpotter() {
+        System.out.println("Keyword spotter created");
+
+        plug = new JavaAIRPlug("KeywordSpotter", "localhost", 10000);
+        plug.init();
+
+        String xml = "<module name=\"JavaPoster\"><post to=\"WB1\" type=\"My.Test.Message.Type\" /></module>";
+
+        plug.sendRegistration(xml);
+
+        plug.postOutputMessage(new Message());
+    }
+
 }
